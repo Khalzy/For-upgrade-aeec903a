27b7baa7a67a6ee94d4cbafad445f9ae00a245fe
Candace <2100978@talnet.nl>
2020-02-03 10:48:26 +0100

    Uitwerking van de exercise

---
+++ upgraded_for.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+$Klasgenoten = array("Sjonnie de Wiel", "Herman Kaal", "Henk de Steen", "Inge Kerkhoven", "Gert Kruiswijk");
+
+
+foreach ($Klasgenoten as $value) {
+    print_r($value . "\n");
+}
